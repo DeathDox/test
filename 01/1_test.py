43e6ee385fb1da2102357fde89593c8e1bd7f27a
DeathDox <81355780+DeathDox@users.noreply.github.com>
2021-03-25 12:15:38 +0300

    Add files via upload

---
+++ 01/1_test.py
@@ -0,0 +1,37 @@
+def banner():
+    system("cls" if name == "nt" else "clear")
+    print(BRIGHT + GREEN)
+    print(r"  ")
+    print(r"  ")
+    print(r"  ")
+    print(r"  ")
+    print()
+    print(r"          ")
+    print(RESET_ALL)
+
+
+def main():
+    banner()
+    print("[1] Поиск по телефону")
+    print("[2] Поиск по голосу")
+    print("[3] ВЫХОД.")
+    print()
+    face = input(f"{BRIGHT}{BLUE}Введите номер пункта: {RESET_ALL}")
+    if face== "1":
+        facefind()
+    elif face == "2":
+        single()
+    elif face == "3":
+        print()
+        exit()
+    else:
+        print(f"\n{BRIGHT}{RED}[*] Номер пункта введён неверно!{RESET_ALL}")
+        sleep(1)
+        main()
+
+
+        def face():
+            check_internet()
+    check_version()
+    banner()
+    print("ТЕХНИЧЕСКИЙ РАБОТЫ")
